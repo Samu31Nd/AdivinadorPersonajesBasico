988f74561235621beb7c6e77d05079b1099fa8c4
SaeSnd <samuelslg03idk@gmail.com>
2024-10-21 21:33:16 -0600

    Se añadieron imagenes

--- src/main.js
+++ src/main.js
@@ -5,15 +5,18 @@ import conocimiento from './baseConocimiento.json';
 
 // Obtener los elementos del DOM
 const preguntaEl = document.getElementById('pregunta');
+const divImagen = document.getElementById('divImagen');
 const botonSi = document.getElementById('botonSi');
 const botonNo = document.getElementById('botonNo');
 const botonReiniciar = document.getElementById('botonReiniciar');
+const image = document.createElement('img');
 
 let nodoActual = conocimiento;
 
 const actualizarPregunta = () => {
   if (nodoActual.personaje) {
     preguntaEl.textContent = `Creo que tu personaje es: ${nodoActual.personaje}`;
+    addImage(nodoActual.personaje);
     botonSi.style.display = 'none';
     botonNo.style.display = 'none';
     botonReiniciar.style.display = 'block';
@@ -21,6 +24,22 @@ const actualizarPregunta = () => {
     preguntaEl.textContent = nodoActual.pregunta;
   }
 }
+
+const addImage = (personaje) => {
+  let imgSrc = personaje.trim().split(" ");
+  imgSrc = imgSrc[0].toLowerCase();
+  divImagen.innerHTML = "";
+  divImagen.style.display = 'block';
+  image.alt =personaje;
+  if(imgSrc == "yapdollar") image.src = `/${imgSrc}.gif`;
+  else
+    image.src = `../public/${imgSrc}.png`;
+  divImagen.append(image);
+}
+
+const hideImage = () => { divImagen.innerHTML = ""; 
+  divImagen.style.display = 'none';
+};
 
 const manejarRespuesta = (respuesta) => {
   if (respuesta === 'si') {
@@ -36,13 +55,14 @@ const reiniciarPrograma = () => {
   botonSi.style.display = 'inline-block'; 
   botonNo.style.display = 'inline-block';
   botonReiniciar.style.display = 'none';  
+  hideImage();
   actualizarPregunta();  
 }
 
 
 botonSi.addEventListener('click', () => manejarRespuesta('si'));
 botonNo.addEventListener('click', () => manejarRespuesta('no'));
-botonReiniciar.addEventListener('click', reiniciarPrograma());
+botonReiniciar.addEventListener('click', () => reiniciarPrograma());
 
 actualizarPregunta();
 
